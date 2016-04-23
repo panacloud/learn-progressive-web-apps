7aba535365f7c724b01738db8464cb0729a64f7a
Zia Khan <ziaukhan@hotmail.com>
2016-04-23 14:02:58 +0500

    added step 5

---
+++ step05_service_worker_events/sw.js
@@ -0,0 +1,12 @@
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
+});
